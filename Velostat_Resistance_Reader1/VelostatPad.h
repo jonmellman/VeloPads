9fd6d840147f67fa2d4749409f6a155ad8cc88fd
Jon Mellman <jonmellman@gmail.com>
2019-08-07 16:19:19 -0700

    Reformat, add VelostatPad.h and initialize it in a velostatPads array

---
+++ Velostat_Resistance_Reader1/VelostatPad.h
@@ -0,0 +1,6 @@
+struct VelostatPad {
+  int analogPinNumber;
+  int currentVelocity;
+  bool isOn;
+  int counter;
+};
--- Velostat_Resistance_Reader1/Velostat_Resistance_Reader1.ino
+++ Velostat_Resistance_Reader1/Velostat_Resistance_Reader1.ino
@@ -3,67 +3,69 @@
 #include <frequencyToNote.h>
 #include <MIDIUSB_Defs.h>
 #include <MIDIUSB.h>
+#include "VelostatStruct.h"
 
 const int NUM_PINS = 2;
-const int analogPins[NUM_PINS] = {0, 1};
 const int Vin = 5;
 const float REFERENCE_RESISTANCE_OHMS = 10000; // Value of the reference resistor on the breadboard.
 const float MAX_RESISTANCE_OHMS = 8000; // Arbitrary, but somewhat calibrated to R1 of 10000
 
+struct VelostatPad velostatPads[NUM_PINS];
 int playing = 0;
 int velocity = 0;
 
-void setup()
-{
-  Serial.begin(115200);
+void setup() {
+    Serial.begin(115200);
+
+    for (int i = 0; i < NUM_PINS; i++) {
+        struct VelostatPad pad = {
+            i,
+            0,
+            false,
+            0
+        };
+
+        velostatPads[i] = pad;
+    }
 }
 
-void loop()
-{
-//  for (int i = 0; i < NUM_PINS; i++) {
-//    float resistance = readResistanceFromPin(analogPins[i]);
-//    Serial.print(formatOutput(analogPins[i], resistance));
-//    Serial.print("   ");
-//  }
+void loop() {
+    float resistance = readResistanceFromPin(0); // TODO: Move to loop
 
-  float resistance = readResistanceFromPin(analogPins[0]);
-
-  if (!playing && resistance < 600) {
-    playing = 1;
-    noteOn(0, 48, 127);
-    Serial.println("note on");
-    Serial.println(resistance);
-    delay(5);
-  } else if (playing && resistance > 700) {
-    playing = 0;
-    noteOff(0, 48, 0);
-    delay(5);
-  }
+    if (!playing && resistance < 600) {
+        playing = 1;
+        noteOn(0, 48, 127);
+        Serial.println("note on");
+        Serial.println(resistance);
+        delay(5);
+    } else if (playing && resistance > 700) {
+        playing = 0;
+        noteOff(0, 48, 0);
+        delay(5);
+    }
 }
 
-float readResistanceFromPin(int pinNumber)
-{
-  float rawAnalogInput = analogRead(pinNumber);
+float readResistanceFromPin(int pinNumber) {
+    float rawAnalogInput = analogRead(pinNumber);
 
-  if (rawAnalogInput == 0) {
-    return MAX_RESISTANCE_OHMS;
-  }
+    if (rawAnalogInput == 0) {
+        return MAX_RESISTANCE_OHMS;
+    }
 
-  float buffer = rawAnalogInput * Vin;
-  float Vout = buffer / 1023.0;
-  
-  buffer = (Vin/Vout) - 1;
-  
-  float outputResistanceOhms = REFERENCE_RESISTANCE_OHMS * buffer;
-  outputResistanceOhms = min(outputResistanceOhms, MAX_RESISTANCE_OHMS); // Clamp output value
+    float buffer = rawAnalogInput * Vin;
+    float Vout = buffer / 1023.0;
 
-  return outputResistanceOhms;
+    buffer = (Vin/Vout) - 1;
+
+    float outputResistanceOhms = REFERENCE_RESISTANCE_OHMS * buffer;
+    outputResistanceOhms = min(outputResistanceOhms, MAX_RESISTANCE_OHMS); // Clamp output value
+
+    return outputResistanceOhms;
 }
 
-String formatOutput(int analogPin, float resistance)
-{
+String formatOutput(int analogPin, float resistance) {
 //  return String(resistance);
-  return String(analogPin) + ": " + String(resistance);
+    return String(analogPin) + ": " + String(resistance);
 }
 
 
@@ -74,13 +76,13 @@ String formatOutput(int analogPin, float resistance)
 // Fourth parameter is the velocity (64 = normal, 127 = fastest).
 
 void noteOn(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOn);
-  MidiUSB.flush();
+    midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
+    MidiUSB.sendMIDI(noteOn);
+    MidiUSB.flush();
 }
 
 void noteOff(byte channel, byte pitch, byte velocity) {
-  midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
-  MidiUSB.sendMIDI(noteOff);
-  MidiUSB.flush();
+    midiEventPacket_t noteOff = {0x08, 0x80 | channel, pitch, velocity};
+    MidiUSB.sendMIDI(noteOff);
+    MidiUSB.flush();
 }
