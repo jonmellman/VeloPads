a428451d60e47305b922759bb7b7623f46aa7eff
Jon Mellman <jonmellman@gmail.com>
2019-08-20 08:57:00 -0700

    Misc cleanup

--- Velostat_Resistance_Reader1/Velostat_Resistance_Reader1.ino
+++ Velostat_Resistance_Reader1/Velostat_Resistance_Reader1.ino
@@ -5,18 +5,18 @@
 #include <MIDIUSB.h>
 #include "VelostatPad.h"
 
-const int NUM_PINS = 1;
-const int V_IN = 5;
+const int NUM_VELOSTAT_PADS = 1; // Adjust based on the analog inputs wired in your circuit.
+const int V_IN = 5; // Input voltage. Typically 5V.
 const float REFERENCE_RESISTANCE_OHMS = 10000; // Value of the reference resistor on the breadboard.
-const float MAX_RESISTANCE_OHMS = 8000;        // Arbitrary, but somewhat calibrated to R1 of 10000
+const float MAX_RESISTANCE_OHMS = 8000; // Arbitrary, but somewhat calibrated to the reference resistor and our velostat pad.
 
-struct VelostatPad velostatPads[NUM_PINS];
+struct VelostatPad velostatPads[NUM_VELOSTAT_PADS];
 
 void setup()
 {
     Serial.begin(115200);
 
-    for (int i = 0; i < NUM_PINS; i++)
+    for (int i = 0; i < NUM_VELOSTAT_PADS; i++)
     {
         struct VelostatPad pad = {
             i,
@@ -30,12 +30,10 @@ void setup()
 
 void loop()
 {
-    for (int i = 0; i < NUM_PINS; i++)
+    for (int i = 0; i < NUM_VELOSTAT_PADS; i++)
     {
         updateState(&velostatPads[i]);
     }
-
-    // delay(5); // For debugging
 }
 
 void updateState(struct VelostatPad *padPointer)
@@ -43,13 +41,11 @@ void updateState(struct VelostatPad *padPointer)
     const int RESISTANCE_THRESHOLD = 6000;
     const int COUNTER_MAX = 5;
     float resistance = readResistanceFromPin(padPointer->analogPinNumber);
-    Serial.println(resistance);
-
-    // TODO: Get velocity at this point, and remove the resistance < RESISTANCE_THRESHOLD check
+    // TODO: Refactor to get velocity at this point, and remove the resistance < RESISTANCE_THRESHOLD check
 
     if (!padPointer->isOn && resistance < RESISTANCE_THRESHOLD)
     {
-        // Now it's on
+        // Now it's on.
         if (padPointer->counter++ >= COUNTER_MAX)
         {
             noteOn(0, 36, padPointer->currentVelocity);
@@ -61,13 +57,12 @@ void updateState(struct VelostatPad *padPointer)
         {
             // Over 5 steps, we set currentVelocity to max of previous velocity and current velocity
             padPointer->currentVelocity = max(getVelocityFromResistance(resistance), padPointer->currentVelocity);
-            // Serial.println(padPointer->currentVelocity);
         }
     }
 
     if (resistance >= RESISTANCE_THRESHOLD)
     {
-        // Now it's off
+        // Now it's off.
         padPointer->currentVelocity = 0;
         padPointer->counter = 0;
         if (padPointer->isOn)
@@ -80,6 +75,7 @@ void updateState(struct VelostatPad *padPointer)
 
 int getVelocityFromResistance(float resistance)
 {
+    // TODO: Calibrate this formula to the resistance min/max of each VeloPad.
     return 127 - (resistance / MAX_RESISTANCE_OHMS) * 127;
 }
 
@@ -103,18 +99,12 @@ float readResistanceFromPin(int pinNumber)
     return outputResistanceOhms;
 }
 
-String formatOutput(int analogPin, float resistance)
-{
-    //  return String(resistance);
-    return String(analogPin) + ": " + String(resistance);
-}
 
 // First parameter is the event type (0x09 = note on, 0x08 = note off).
 // Second parameter is note-on/note-off, combined with the channel.
 // Channel can be anything between 0-15. Typically reported to the user as 1-16.
 // Third parameter is the note number (48 = middle C).
 // Fourth parameter is the velocity (64 = normal, 127 = fastest).
-
 void noteOn(byte channel, byte pitch, byte velocity)
 {
     midiEventPacket_t noteOn = {0x09, 0x90 | channel, pitch, velocity};
